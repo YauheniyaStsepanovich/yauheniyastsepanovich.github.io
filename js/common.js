cc4a2a44c4126d4cecf662adab7a3a7657d38946
YauheniyaStsepanovich <36709198+YauheniyaStsepanovich@users.noreply.github.com>
2018-06-12 15:58:03 +0300

    cc

--- js/common.js
+++ js/common.js
@@ -13,26 +13,24 @@ $(function() {
 });
 
 
-
-
 $(document).ready(function() {
 
   ymaps.ready(init);
-        var myMap, 
-            myPlacemark;
+    var myMap, 
+    myPlacemark;
 
-        function init(){ 
-            myMap = new ymaps.Map("map", {
-                center: [55.76, 37.64],
-                zoom: 7
-            }); 
-            
-            myPlacemark = new ymaps.Placemark([55.76, 37.64], {
-                hintContent: 'Москва!',
-                balloonContent: 'Столица России'
-            });
-            
-            myMap.geoObjects.add(myPlacemark);
+    function init(){ 
+      myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 7
+        }); 
+  
+        myPlacemark = new ymaps.Placemark([55.76, 37.64], {
+          hintContent: 'Москва!',
+          balloonContent: 'Столица России'
+          });
+  
+        myMap.geoObjects.add(myPlacemark);
         }
 
 	$("#menu a, .top").mPageScroll2id({
