07593ffdf1e584548b7923ac597a841f4a22fd09
YauheniyaStsepanovich <36709198+YauheniyaStsepanovich@users.noreply.github.com>
2018-06-12 15:38:36 +0300

    сс

--- js/common.js
+++ js/common.js
@@ -74,3 +74,20 @@ $(".loaderInner").fadeOut();
 $(".loader").delay(400).fadeOut("slow");
 });
 
+ymaps.ready(init);
+    var myMap,
+        myPlacemark;
+
+    function init(){     
+        myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 7
+        });
+
+        myPlacemark = new ymaps.Placemark([55.76, 37.64], { 
+            hintContent: 'Москва!', 
+            balloonContent: 'Столица России' 
+        });
+
+        myMap.geoObjects.add(myPlacemark);
+    }
